1e67b0cf6aa5b78a3d40d50c663f5bff0dee0432
Michael Aufreiter <michael@substance.io>
2012-09-19 13:34:28 +0200

    Deal with a lot of special cases.
    Also check doc against an invariant after a new op has been applied.

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var _        = require('underscore');
+var fs       = require('fs');
+var assert   = require('assert');
+var Document = require('../document').Document;
+var schema   = JSON.parse(fs.readFileSync(__dirname+ '/../data/substance.json', 'utf-8'));
+
+var emptyDoc = JSON.parse(fs.readFileSync(__dirname+ '/fixtures/busy_doc.json', 'utf-8'));
+
+// Just runs all the ops and checks the doc state against an invariant all the time.
+var doc = new Document(emptyDoc, schema);
+
