0f3f22381a6f2e5f611be698a62e27ee48c4c539
Michael Aufreiter <michael@substance.io>
2013-08-14 13:56:01 +0200

    Writer -> Controller.

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var Document = require('./src/document');
 Document.Annotator = require('./src/annotator');
 Document.Cursor = require('./src/cursor');
 Document.Selection = require('./src/selection');
-Document.Writer = require('./src/writer');
+Document.Controller = require('./src/controller');
 Document.Transformer = require('./src/transformer');
+
+// Compatibility
+Document.Writer = require('./src/controller');
 
 module.exports = Document;
