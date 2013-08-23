25664a9b63dc5f28ba3ca2561f1e6e2950293a01
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-08-23 20:42:40 +0200

    Enable Container tests.

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,16 @@
 "use strict";
 
-//require("./container_test");
-
 // Document Tests
 // ---------------
 
 //require("./document_test");
 
 require("./document_manipulation_test");
+
+// Container Tests
+// --------
+
+require("./container_test");
 
 // Selection Tests
 // ---------------
