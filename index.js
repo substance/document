65ef0156bd1c2c292d28aa29021f58987d5efe62
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-12-31 20:00:35 +0200

    Removed obsolete DocumentController.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var _ = require("underscore");
 
 var Document = require('./src/document');
 Document.Annotator = require('./src/annotator');
-Document.Controller = require('./src/document_controller');
 Document.Cursor = require('./src/cursor');
 Document.Selection = require('./src/selection');
 
