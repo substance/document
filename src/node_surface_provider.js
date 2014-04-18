8e5700335df3e0a368a84fd8dbd03480a59b426e
Oliver Buchtala <oliver.buchtala@googlemail.com>
2014-04-18 22:26:00 +0200

    Bugfix: fixes a severe issue witch cached node surfaces.

--- src/node_surface_provider.js
+++ src/node_surface_provider.js
@@ -1,14 +1,26 @@
 "use strict";
 
 var _ = require("underscore");
+var util = require("substance-util");
 
 var NodeSurfaceProvider = function(doc) {
   this.document = doc;
   this.nodeTypes = this.document.nodeTypes;
   this.nodeSurfaces = {};
+
+  this.listenTo(this.document, "operation:applied", this.onGraphUpdate);
 };
 
 NodeSurfaceProvider.Prototype = function() {
+
+  // ATTENTION: the caching here needs invalidation!
+  // I.e., what happens if you delete a node and create an instance with the same id?
+  // then the cache must be invalidated and deliver a new node surface on next request.
+  this.onGraphUpdate = function(op) {
+    if (op.type === "delete") {
+      delete this.nodeSurfaces[op.path[0]];
+    }
+  }
 
   this.getNodeSurface = function(node_or_nodeId) {
     var nodeId, node;
@@ -18,6 +30,7 @@ NodeSurfaceProvider.Prototype = function() {
       node = node_or_nodeId;
       nodeId = node.id;
     }
+
 
     if (!this.nodeSurfaces[nodeId]) {
       node = node || this.document.get(nodeId);
@@ -57,6 +70,7 @@ NodeSurfaceProvider.Prototype = function() {
   };
 
 };
+NodeSurfaceProvider.Prototype.prototype = util.Events;
 NodeSurfaceProvider.prototype = new NodeSurfaceProvider.Prototype();
 
 NodeSurfaceProvider.EmptySurface = function(node) {
